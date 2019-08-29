438ff5ed4af22452157f5fced1e7a49a2155011a
Ka Leong Tong <katong@calpoly.edu>
2019-08-29 08:29:48 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState, useReducer } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.scss";
 import CardGridComponent from "./components/molecule/CardGridComponent";
 import NavigationComponent from "./components/molecule/NavigationComponent";
 import ShoppingSideBar from "./components/molecule/ShoppingSideBarComponent";
 import UserSideBar from "./components/molecule/UserSideBarComponent";
 import { initialCartState, cartReducer } from "./reducers/cartReducer";
+import RegisterComponent from "./components/molecule/RegisterComponent";
+import LoginComponent from "./components/molecule/LoginComponent";
 const data = require("./data.json");
 
 function App() {
@@ -24,23 +27,33 @@ function App() {
   const addItem = id => cartDispatch({ type: "add", payload: { id } });
 
   return (
-    <div className="container">
-      <NavigationComponent
-        toggleShoppingSideBar={toggleShoppingSideBar}
-        toggleUserSideBar={toggleUserSideBar}
-      />
-      <div className="content">
-        {shoppingSideBarOpen && (
-          <ShoppingSideBar
-            state={cartState}
-            items={orderList}
-            dispatch={cartDispatch}
-          />
-        )}
-        {userSideBarOpen && <UserSideBar />}
-        <CardGridComponent orderList={orderList} addItem={addItem} />
+    <BrowserRouter>
+      <div className="container">
+        <NavigationComponent
+          toggleShoppingSideBar={toggleShoppingSideBar}
+          toggleUserSideBar={toggleUserSideBar}
+        />
+        <Route path="/register" component={RegisterComponent} />
+        <Route path="/login" component={LoginComponent} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="content">
+              {shoppingSideBarOpen && (
+                <ShoppingSideBar
+                  state={cartState}
+                  items={orderList}
+                  dispatch={cartDispatch}
+                />
+              )}
+              {userSideBarOpen && <UserSideBar />}
+              <CardGridComponent orderList={orderList} addItem={addItem} />
+            </div>
+          )}
+        />
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
